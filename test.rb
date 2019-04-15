f48afe14dd84ba292fd42f184141ec5b4f68c542
Luke O'Malley <lukeomalley95@gmail.com>
2019-04-15 21:24:56 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,18 @@
+birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+}
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each {
+    |name, age|
+    if age <= 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  }
+end
+
+happy_birthday(birthday_kids)
